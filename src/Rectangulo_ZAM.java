e51eceb66683cf0e910e82aac6551ba6face3be5
Zoraida Alonso <z.alonso.m@gmail.com>
2018-05-09 14:13:11 +0200

    versión comentada

--- src/Rectangulo_ZAM.java
+++ src/Rectangulo_ZAM.java
@@ -1,8 +1,19 @@
 
+/**
+  * Clase rectangulo
+  * @author Zoraida Alonso Muñoz
+  * @version 1.1
+  */
 public class Rectangulo_ZAM extends FiguraGeometrica_ZAM {
 	private double l1;
 	private double l2;
 	
+	/**
+	  * Metodo para definir un rectangulo
+	  * @param tipoFigura (rectangulo, circulo o triangulo)
+	  * @param lG longitud lado grande
+	  * @param lP longitud lado pequeño
+	  */
 	public Rectangulo_ZAM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
@@ -10,11 +21,21 @@ public class Rectangulo_ZAM extends FiguraGeometrica_ZAM {
 	}
 
 	@Override
+	/**
+	  * Metodo que calcula el area de un rectangulo
+	  * @param l1 longitud lado grande
+	  * @param l2 longitud lado pequeño
+	  */
 	public double area() {
 		return l1 * l2;
 	}
 	
 	@Override
+	/**
+	  * Metodo que calcula el perimetro de un rectangulo
+	  * @param l1 longitud lado grande
+	  * @param l2 longitud lado pequeño
+	  */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
